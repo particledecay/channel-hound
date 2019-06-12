a468e688adb505287cf81d8dac279257c203cc1f
Joey Espinosa <jlouis.espinosa@gmail.com>
2019-06-12 00:02:47 -0400

    refactor: make YouTube TV price dynamic

--- channel_hound/channel_hound/spiders/youtubetv.py
+++ channel_hound/channel_hound/spiders/youtubetv.py
@@ -11,26 +11,28 @@ class YoutubetvSpider(scrapy.Spider):
     name = 'youtubetv'
     allowed_domains = ['tv.youtube.com', 'youtube-tv-zip-lookup.appspot.com']
     start_urls = [
-        'https://youtube-tv-zip-lookup.appspot.com/zipLookup/v1/availability/33065',
+        'https://tv.youtube.com/welcome'
     ]
     service = 'YouTube TV'
 
     def parse(self, response):
+        premium_row = response.xpath('//ul[contains(@class, "network-matrix__cells--premium")]/li')
+        for premium_element in premium_row:
+            package = {'service': YoutubetvSpider.service, 'name': 'Add-On'}
+            package['price'] = premium_element.xpath('substring-after(./div[@class="caption"]/text(), "$")').extract()[0].replace("/mo", "").strip()
+            name = premium_element.xpath('./div[contains(@class, "network-matrix__cells-cell-cloak")]/@aria-label').extract()[0].strip()
+
+            yield ChannelItem(name=name, package=package)
+
+        base_price = response.xpath('substring-after(substring-before(//p[@class="c-hero-banner__content-price"]/text(), "/"), "$")').extract()[0].strip()
+        yield Request('https://youtube-tv-zip-lookup.appspot.com/zipLookup/v1/availability/33065',
+                      callback=self.parse_base_channels, meta={'price': base_price})
+
+    def parse_base_channels(self, response):
         jsonresponse = json.loads(response.body_as_unicode())
 
         for market in jsonresponse['markets']:
             for channel in market['channels']:
                 if not channel['add_on']:
-                    package = {'service': YoutubetvSpider.service, 'price': '49.99', 'name': 'Base'}
+                    package = {'service': YoutubetvSpider.service, 'price': response.meta['price'], 'name': 'Base'}
                     yield ChannelItem(name=channel['display_name'], package=package)
-
-        yield Request('https://tv.youtube.com/welcome', callback=self.parse_additional)
-
-    def parse_additional(self, response):
-        premium_row = response.xpath('//ul[contains(@class, "network-matrix__cells--premium")]/li')
-        for premium_element in premium_row:
-            package = {'service': YoutubetvSpider.service, 'name': 'Add-On'}
-            package['price'] = premium_element.xpath('substring-after(substring-before(./div[@class="caption"]/text(), "/"), "$")').extract()[0].strip()
-            name = premium_element.xpath('./div[contains(@class, "network-matrix__cells-cell-cloak")]/@aria-label').extract()[0].strip()
-
-            yield ChannelItem(name=name, package=package)
