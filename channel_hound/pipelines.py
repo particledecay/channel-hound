8810e546a7048b9d6f3110e33c2841573bbd683b
Joey Espinosa <jlouis.espinosa@gmail.com>
2019-06-12 23:45:03 -0400

    feat: add pipeline for file writing

--- channel_hound/pipelines.py
+++ channel_hound/pipelines.py
@@ -14,6 +14,22 @@ from scrapy.exporters import JsonItemExporter
 from services.models import Channel, Package, Service
 
 
+class JsonPipeline(object):
+
+    def __init__(self):
+        self.file = open('channels.json', 'wb')
+        self.exporter = JsonItemExporter(self.file)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+
 class DatabasePipeline(object):
 
     def process_item(self, item, spider):
@@ -39,3 +55,5 @@ class DatabasePipeline(object):
             chn = Channel(name=item['name'])
             chn.save()
         chn.packages.add(pkg)
+
+        return item
