1e54e7fb0be535162b55dcd5f8dae20dfd640535
Joey Espinosa <jlouis.espinosa@gmail.com>
2019-10-29 16:39:12 -0400

    refactor: remove commented (old) YouTube TV code

--- channel_hound/spiders/youtubetv.py
+++ channel_hound/spiders/youtubetv.py
@@ -16,17 +16,6 @@ class YoutubetvSpider(scrapy.Spider):
     service = 'YouTube TV'
 
     def parse(self, response):
-        # premium_row = response.xpath('//ul[contains(@class, "network-matrix__cells--premium")]/li')
-        # for premium_element in premium_row:
-        #     package = {'service': YoutubetvSpider.service, 'name': 'Add-On'}
-        #     package['price'] = premium_element.xpath('substring-after(./div[@class="caption"]/text(), "$")').extract()[0].replace("/mo", "").strip()
-        #     name = premium_element.xpath('./div[contains(@class, "network-matrix__cells-cell-cloak")]/@aria-label').extract()[0].strip()
-
-        #     yield ChannelItem(name=name, package=package)
-
-        # base_price = response.xpath('substring-after(substring-before(//p[@class="c-hero-banner__content-price"]/text(), "/"), "$")').extract()[0].strip()
-        # yield Request('https://youtube-tv-zip-lookup.appspot.com/zipLookup/v1/availability/33065',
-        #               callback=self.parse_base_channels, meta={'price': base_price})
         all_channels = response.xpath('//div[@class="zip__network"]')
         base_price = response.xpath('substring-after(substring-before(//span[@class="price"]/text(), "/"), "$")').extract()[0].strip()
         for channel in all_channels:
@@ -45,13 +34,6 @@ class YoutubetvSpider(scrapy.Spider):
                 yield Request(channel_page[0], callback=self.parse_base_channels, meta={'name': name, 'base_price': base_price, 'package': package})
 
     def parse_base_channels(self, response):
-        # jsonresponse = json.loads(response.body_as_unicode())
-
-        # for market in jsonresponse['markets']:
-        #     for channel in market['channels']:
-        #         if not channel['add_on']:
-        #             package = {'service': YoutubetvSpider.service, 'price': response.meta['price'], 'name': 'Base'}
-        #             yield ChannelItem(name=channel['display_name'], package=package)
         package = response.meta['package']
         price = response.xpath('substring-after(substring-before(//div[@class="ytv-promo-drawer-text-primary"]/text(), "/"), "$")').extract()
         if not price:
